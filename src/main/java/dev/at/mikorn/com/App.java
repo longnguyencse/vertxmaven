00200e20adf21d677f6fa81b32988e1c6a0d17da
nqlong <longnguyenqu@gmail.com>
2018-08-28 23:01:08 +0700

    Initial commit

---
+++ src/main/java/dev/at/mikorn/com/App.java
@@ -0,0 +1,28 @@
+package dev.at.mikorn.com;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) {
+        vertx
+                .createHttpServer()
+                .requestHandler(r -> {
+                    r.response().end("<h1>Hello from my first " +
+                            "Vert.x 3 application</h1>");
+                })
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        fut.complete();
+                    } else {
+                        fut.fail(result.cause());
+                    }
+                });
+    }
+
+    public static void main(String[] args) {
+//        Run
+    }
+}
